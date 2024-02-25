1b5e49020492d19182dbde3ab2e50e2bb6c586aa
OhRetro <gabrielnacazone@hotmail.com>
2024-02-25 14:52:07 -0300

    25.2.24 - wf3 test

--- components/tools/make_executable.py
+++ components/tools/make_executable.py
@@ -1,11 +1,11 @@
 from os import system
 
 def make_executable(version: list[int]):
-    options = "--standalone --onefile --disable-ccache"
+    options = "--standalone --onefile"
     options += " --company-name=OhRetro"
     options += " --product-name=Nakathon"
     options += f" --file-version={version}"
-    
+    options += " --output-dir=build/"
     options += " --windows-icon-from-ico=logo.ico"
 
     system(f"nuitka {options} nakathon.py")
